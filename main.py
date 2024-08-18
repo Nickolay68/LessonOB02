680d8bd1cbdbf15f1bf66bd2d0e620584b27f7c7
Nickolay68/LessonOB02 <p.nick21vek@gmail.com>
2024-08-18 15:38:08 +0300

    add ?

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ class User:
         self._user_id = user_id
         self._name = name
         self._access_level = 'user'
+        self._age = name
+
+    def _User__age(self):
+            return self.__age
+
+
 
     # Методы для получения значений атрибутов
     def get_user_id(self):
@@ -54,15 +60,19 @@ if __name__ == "__main__":
     # Создаем обычных пользователей
     user1 = User(2, "Николай")
     user2 = User(3, "Володя")
+    user3 = User(4, "Олег")
+    user4 = User(5, "Настя")
 
     # Администратор добавляет пользователей в список
     admin.add_user(users_list, user1)
     admin.add_user(users_list, user2)
+    admin.add_user(users_list, user3)
+    admin.add_user(users_list, user4)
 
     # Выводим информацию о всех пользователях в системе
     print("\nТекущие пользователи в системе:")
     for user in users_list:
-        print(f"ID: {user.get_user_id()}, Имя: {user.get_name()}, Уровень доступа: {user.get_access_level()}")
+        print(f"ID: {user.get_user_id()}, Имя: {user.get_name()}, Уровень доступа: {user.get_access_level()}, Возраст: {user._User__age()}")
 
     # Администратор удаляет пользователя по ID
     admin.remove_user(users_list, 2)
